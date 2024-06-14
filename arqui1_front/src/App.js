670b0da70341c562b420afbc1f843e1f8d062493
betebetoven <albertoha2000@gmail.com>
2024-06-14 17:05:29 -0600

    inicial

---
+++ arqui1_front/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WebSocketComponent from './components/WebSocketComponent';
+function App() {
+  return (
+      <div className="App">
+          <WebSocketComponent />
+      </div>
+  );
+}
+
+export default App;
